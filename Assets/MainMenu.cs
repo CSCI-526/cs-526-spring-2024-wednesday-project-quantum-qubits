7860dab8a79dae4de355b7dcd632fa5d5a91065e
Tiffany Chen <tchen874@usc.edu>
2024-03-26 23:47:27 -0700

    menu page

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayLevel1Game()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void PlayLevel2Game()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+    
+}
